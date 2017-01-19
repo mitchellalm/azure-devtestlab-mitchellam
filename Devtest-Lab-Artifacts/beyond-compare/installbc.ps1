1c15430c8800a0c94f1668c81388650c44b33dd5
cn101028 <cn101028@CN101028>
2017-01-19 14:46:33 -0800

    Initial checkin for Beyond compare artificat

---
+++ Devtest-Lab-Artifacts/beyond-compare/installbc.ps1
@@ -0,0 +1,20 @@
+﻿$bcSetupUrl = 'http://www.scootersoftware.com/BCompare-4.1.3.20814.exe'
+$bcSetup = "${env:Temp}\BCompare-4.1.3.20814.exe"
+
+try
+{
+    Invoke-WebRequest -Uri $bcSetupUrl -OutFile $bcSetup
+}
+catch
+{
+    Write-Error "Failed to download BeyondCompare 4 Setup"
+}
+
+try
+{
+    Start-Process -FilePath $bcSetup
+}
+catch
+{
+    Write-Error 'Failed to install BeyondCompare 4'
+}
